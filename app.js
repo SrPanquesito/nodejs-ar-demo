9c981ab84082f01ef1d46bb095e43e3c50adecd5
SrPanquesito <luisgdiazreynoso@gmail.com>
2020-02-28 02:23:50 -0800

    Base

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// Routes
+const home = require('./routes/home');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(home);
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(8000);
